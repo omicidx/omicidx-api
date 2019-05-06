b0e0fd43471ae71f6d78573d299cba03f78aff6c
Sean Davis <seandavi@gmail.com>
2019-05-06 12:50:56 -0400

    basic study search (query_string) working

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from fastapi import Query
+from fastapi import (FastAPI,
+                     HTTPException,
+                     Query,
+                     )
 from .esclient import ESClient
 from elasticsearch_dsl import Search
 import elasticsearch
@@ -14,7 +16,7 @@ async def root():
 
 
 @app.get("/study/{accession}")
-async def get_study_accession(accession: str):
+async def get_study_accession(accession: str ):
     try:
         return es.client.get(index="sra_study", doc_type="doc", id=accession)
     except elasticsearch.exceptions.NotFoundError as e:
@@ -23,3 +25,10 @@ async def get_study_accession(accession: str):
             detail = f"Accession {accession} not found."
         )
     
+@app.get("/studies/search")
+async def search_study(q: str = Query(None),
+                       size: int = Query(10, gte=0, lt=1000),
+):
+    search = Search(using = es.client)
+    resp = search.index('sra_study').query('query_string', query = q).execute()
+    return resp[0:size]
--- tests/test_sra.py
+++ tests/test_sra.py
@@ -19,3 +19,27 @@ def test_nonexistent_study():
     response = client.get("/study/nonexistent_study")
     assert response.status_code == 404
     assert response.json() == {"detail": "Accession nonexistent_study not found."}
+
+def test_studies_search_works():
+    response = client.get("/studies/search?q=cancer&size=5")
+    assert response.status_code == 200
+    assert len(response.json()['_l_']) == 5
+
+def test_studies_search_limit_size():
+    response = client.get("/studies/search?q=cancer&size=10000")
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "ctx": {
+                    "limit_value": 1000
+                },
+                "loc": [
+                    "query",
+                    "size"
+                ],
+                "msg": "ensure this value is less than 1000",
+                "type": "value_error.number.not_lt"
+            }
+        ]
+    }
