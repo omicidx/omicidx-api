8dbd8e8827742b8b894e19db620d5fdd6743d14f
Sean Davis <seandavi@gmail.com>
2019-05-06 11:41:13 -0400

    simple setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='omicidx-web',
+      version='0.1',
+      description='Runs the omicidx website and api',
+      author='Sean Davis',
+      author_email='seandavi@gmail.com',
+      packages=['app'],
+     )
