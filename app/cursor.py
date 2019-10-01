bc43a885a5fd470a020319b851daf935ec0beccd
Sean Davis <seandavi@gmail.com>
2019-10-01 11:27:09 -0400

    Implementation of cursor for search_after functionality
    * Still need to implement usage

---
+++ app/cursor.py
@@ -0,0 +1,31 @@
+"""enconde and decode a cursor
+
+Given a list of sort items (dicts) and a tiebreaker ID,
+create a cursor that can be encoded/decoded for use in 
+search_after (or search_before) queries.
+
+>>> encode_cursor([{'_score':'desc'},{'accession': 'asc'}],'SRP00001')
+b'X3Njb3JlfHxkZXNjfHx8YWNjZXNzaW9ufHxhc2N8fHx8U1JQMDAwMDE=\n'
+
+>>>  decode_cursor(b'X3Njb3JlfHxkZXNjfHx8YWNjZXNzaW9ufHxhc2N8fHx8U1JQMDAwMDE=\n')
+([{b'_score': b'desc'}, {b'accession': b'asc'}], b'SRP00001')
+"""
+import base64
+
+
+def encode_cursor(sort_dict: list, resp) -> bytes:
+    sort_items = []
+    for d in sort_dict:
+        sort_items.append("{}||{}".format(*d.popitem()))
+    cursor_string = "|||".join(sort_items) + "||||{}".format(resp)
+    return base64.encodebytes(cursor_string.encode('UTF-8'))
+
+
+
+def decode_cursor(cursor_string: bytes):
+    (kvs, id) = base64.decodebytes(cursor_string).split(b"||||")
+    sort_items = kvs.split(b'|||')
+    sort_dict = []
+    for item in sort_items:
+        sort_dict.append(dict((item.split(b'||'),)))
+    return (sort_dict, id)
