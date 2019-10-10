2c0c05262d889c250485c7987b3bd5c3307a0954
Sean Davis <seandavi@gmail.com>
2019-10-10 15:32:32 -0400

    switching over to gcp from aws kubernetes

---
+++ build_and_push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t seandavi/omidicx-fastapi .
+docker tag seandavi/omidicx-fastapi gcr.io/isb-cgc-01-0006/omicidx-fastapi
+docker push gcr.io/isb-cgc-01-0006/omicidx-fastapi
+kubectl get pods | grep omicidx-fastapi | awk '{print $1}' | xargs kubectl delete pod
