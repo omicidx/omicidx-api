c14b8d787a3139f1263007da8e19db6e4ecea4e5
Sean Davis <seandavi@gmail.com>
2020-03-16 19:57:25 -0400

    switch go cloud run from kubernetes
    going serverless

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+REVISION=`git rev-parse HEAD`
 docker build -t seandavi/omidicx-fastapi .
 docker tag seandavi/omidicx-fastapi gcr.io/isb-cgc-01-0006/omicidx-fastapi:latest
-docker tag seandavi/omidicx-fastapi gcr.io/isb-cgc-01-0006/omicidx-fastapi:`git rev-parse HEAD`
+docker tag seandavi/omidicx-fastapi gcr.io/isb-cgc-01-0006/omicidx-fastapi:$REVISION
 docker push gcr.io/isb-cgc-01-0006/omicidx-fastapi:latest
-docker push gcr.io/isb-cgc-01-0006/omicidx-fastapi:`git rev-parse HEAD`
-kubectl get pods | grep omicidx-fastapi | awk '{print $1}' | xargs kubectl delete pod
+docker push gcr.io/isb-cgc-01-0006/omicidx-fastapi:$REVISION
+#kubectl get pods | grep omicidx-fastapi | awk '{print $1}' | xargs kubectl delete pod
+gcloud run deploy omicidx-fastapi --image gcr.io/isb-cgc-01-0006/omicidx-fastapi:$REVISION
+
